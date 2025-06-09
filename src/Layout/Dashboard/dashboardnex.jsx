47dd73283028b345ace3c5539dff456c653fdb99
shamim-kabir-kazim <shamim_kabir_kazim@proton.me>
2025-06-09 16:24:38 +0000

    frontend

--- src/Layout/Dashboard/dashboardnex.jsx
+++ src/Layout/Dashboard/dashboardnex.jsx
@@ -1,126 +1,70 @@
 import React, { useState } from "react";
 import "./dashboardnex.css";
+import {
+  MdDashboard,
+  MdWorkOutline,
+  MdOutlineLibraryAdd,
+  MdGroup,
+  MdPerson,
+  MdMessage,
+  MdAssignment,
+  MdPayment,
+  MdHelpOutline,
+  MdInfoOutline,
+  MdNotificationsNone,
+  MdSearch
+} from "react-icons/md";
 
 const menuData = [
   {
     label: "Dashboard",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><path d="M12 3l10 9h-3v9h-14v-9h-3z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><path d="M12 3l10 9h-3v9h-14v-9h-3z"/></svg>
-      ),
-    },
+    icon: <MdDashboard size={18} />,
   },
   {
     label: "Find Work",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" fill="black" width="18" height="18" viewBox="0 0 24 24"><path d="M10 2a8 8 0 105.293 14.293l5.707 5.707-1.414 1.414-5.707-5.707A8 8 0 1010 2zm0 2a6 6 0 110 12A6 6 0 0110 4z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" fill="white" width="18" height="18" viewBox="0 0 24 24"><path d="M10 2a8 8 0 105.293 14.293l5.707 5.707-1.414 1.414-5.707-5.707A8 8 0 1010 2zm0 2a6 6 0 110 12A6 6 0 0110 4z"/></svg>
-      ),
-    },
+    icon: <MdWorkOutline size={18} />,
     dropdown: true,
     submenu: ["All", "Applied", "Accepted", "Completed"],
   },
   {
     label: "Post a Job",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><path d="M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z"/></svg>
-      ),
-    },
+    icon: <MdOutlineLibraryAdd size={18} />,
     dropdown: true,
     submenu: ["All", "Active", "Finished"],
   },
   {
     label: "Freelancers",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" fill="black" width="18" height="18" viewBox="0 0 24 24"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5C17 14.17 12.33 13 10 13zm8 0c-.29 0-.62.02-.97.05C18.02 14.21 20 15.25 20 16.5V19h4v-2.5C24 14.17 20.33 13 18 13z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" fill="white" width="18" height="18" viewBox="0 0 24 24"><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5s-3 1.34-3 3 1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5C17 14.17 12.33 13 10 13zm8 0c-.29 0-.62.02-.97.05C18.02 14.21 20 15.25 20 16.5V19h4v-2.5C24 14.17 20.33 13 18 13z"/></svg>
-      ),
-    },
+    icon: <MdGroup size={18} />,
   },
   {
     label: "My Profile",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><circle cx="12" cy="8" r="4"/><path d="M12 14c-4 0-6.5 2-6.5 2v2h13v-2s-2.5-2-6.5-2z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><circle cx="12" cy="8" r="4"/><path d="M12 14c-4 0-6.5 2-6.5 2v2h13v-2s-2.5-2-6.5-2z"/></svg>
-      ),
-    },
+    icon: <MdPerson size={18} />,
     dropdown: true,
     submenu: ["Profile", "Reviews", "Settings"],
   },
   {
     label: "Messages",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><path d="M20 2H4a2 2 0 00-2 2v16l4-4h14a2 2 0 002-2V4a2 2 0 00-2-2z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><path d="M20 2H4a2 2 0 00-2 2v16l4-4h14a2 2 0 002-2V4a2 2 0 00-2-2z"/></svg>
-      ),
-    },
+    icon: <MdMessage size={18} />,
   },
   {
     label: "Projects",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><path d="M10 4H2v16h20V6H12l-2-2z"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><path d="M10 4H2v16h20V6H12l-2-2z"/></svg>
-      ),
-    },
+    icon: <MdAssignment size={18} />,
     dropdown: true,
     submenu: ["Active", "Completed", "Pending"],
   },
   {
     label: "Payments",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><rect x="2" y="6" width="20" height="12" rx="2"/><path d="M2 10h20"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><rect x="2" y="6" width="20" height="12" rx="2"/><path d="M2 10h20"/></svg>
-      ),
-    },
+    icon: <MdPayment size={18} />,
     dropdown: true,
     submenu: ["Transactions", "Withdrawals", "Billing"],
   },
   {
     label: "Support",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="12" r="4"/><path d="M2 12h4m12 0h4M12 2v4m0 12v4M4.93 4.93l2.83 2.83m8.49 8.49l2.83 2.83M19.07 4.93l-2.83 2.83M6.34 17.66l-2.83 2.83"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"/><circle cx="12" cy="12" r="4"/><path d="M2 12h4m12 0h4M12 2v4m0 12v4M4.93 4.93l2.83 2.83m8.49 8.49l2.83 2.83M19.07 4.93l-2.83 2.83M6.34 17.66l-2.83 2.83"/></svg>
-      ),
-    },
+    icon: <MdHelpOutline size={18} />,
   },
   {
     label: "About",
-    icon: {
-      black: (
-        <svg className="icon-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="black" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"/><rect x="11" y="11" width="2" height="6"/><rect x="11" y="7" width="2" height="2"/></svg>
-      ),
-      white: (
-        <svg className="icon-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" viewBox="0 0 24 24"><circle cx="12" cy="12" r="10"/><rect x="11" y="11" width="2" height="6"/><rect x="11" y="7" width="2" height="2"/></svg>
-      ),
-    },
+    icon: <MdInfoOutline size={18} />,
   },
 ];
 
@@ -161,7 +105,7 @@ const DashboardNex = () => {
                 style={{ userSelect: "none" }}
               >
                 <span className="menu-icon">
-                  {selected === item.label ? item.icon.white : item.icon.black}
+                  {item.icon}
                 </span>
                 {item.label}
                 {item.dropdown && (
@@ -212,16 +156,17 @@ const DashboardNex = () => {
                 : selected}
             </h1>
           </div>
-          <input type="text" className="search-bar" placeholder="Search here" />
+          <div className="search-bar-wrapper">
+            <MdSearch size={20} className="search-icon" />
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search dashboard..."
+            />
+          </div>
           <div className="header-user">
             <div className="header-notification">
-              {/* Google Material Notification Icon (Light Blue) */}
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <path
-                  d="M12 22c1.1 0 1.99-.9 1.99-2h-3.98A2 2 0 0 0 12 22zm6-6V9c0-3.07-1.63-5.64-4.5-6.32V2.5a1.5 1.5 0 0 0-3 0v.18C7.64 3.36 6 5.92 6 9v7l-1.99 2c-.01.01-.01.03-.01.04A1 1 0 0 0 5 21h14a1 1 0 0 0 .99-1.09c0-.01 0-.03-.01-.04L18 16z"
-                  fill="#00a4d6"
-                />
-              </svg>
+              <MdNotificationsNone size={26} color="#00a4d6" />
             </div>
             <div className="header-profile">
               <img
